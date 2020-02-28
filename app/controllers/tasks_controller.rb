9f4c0cbbf7eff93c915d22f7a4a06d5e1421a5da
doihy.ogyogyo <dogyogyo117@gmail.com>
2020-02-28 13:58:48 +0000

    user login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,7 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
-    @tasks = Task.order(created_at: :desc).page(params[:page]).per(3)
+    @tasks = current_user.tasks.order(created_at: :desc).page(params[:page]).per(3)
   end
 
   def show
@@ -13,7 +13,7 @@ class TasksController < ApplicationController
     @task = Task.new
   end
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = 'Taskが正常に投稿されました'
@@ -28,9 +28,6 @@ class TasksController < ApplicationController
   end
 
   def update
-    
-
-    
     if @task.update(task_params)
       flash[:success] = 'Taskは正常に更新されました'
       redirect_to @task
@@ -49,10 +46,20 @@ class TasksController < ApplicationController
   private
 
   def set_task
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find_by(id: params[:id])
+    if !@task
+      redirect_to root_url
+    end
   end
 
   def task_params
     params.require(:task).permit(:content, :status)
   end
+
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end  
 end
